52088b2df6aec9e7f5da8d227ae98413dfe1f05e
Milad Adli <milad.adli@devotel.io>
2025-07-07 10:02:53 +0330

    chore: configure modules and integrate global filters and Swagger
    - Register CQRS, TypeORM, and repositories in AppModule
    - Set up global HTTP exception filter
    - Integrate Swagger and enable CORS in main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ConfigModule } from '@nestjs/config';
+import { CustomerOrmEntity } from './infrastructure/database/customer.orm-entity';
+import { TypeOrmCustomerRepository } from './infrastructure/repositories/typeorm-customer.repository';
+import { CustomerController } from './presentation/controllers/customer.controller';
+import { CreateCustomerHandler } from './application/handlers/command/create-customer.handler';
+import { GetCustomersHandler, GetCustomerByIdHandler, GetCustomerByEmailHandler } from './application/handlers/query/get-customers.handler';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forFeature([CustomerOrmEntity]),
+    CqrsModule,
+  ],
+  controllers: [CustomerController],
+  providers: [
+    TypeOrmCustomerRepository,
+    { provide: 'ICustomerRepository', useExisting: TypeOrmCustomerRepository },
+    CreateCustomerHandler,
+    GetCustomersHandler,
+    GetCustomerByIdHandler,
+    GetCustomerByEmailHandler,
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './presentation/swagger/swagger.config';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalFilters(new HttpExceptionFilter());
+  setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
